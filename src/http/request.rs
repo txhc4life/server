3b288ef8a15582b6ed673cb472e19d1d3fe0bc3e
Richard Hinojosa <txhc4life@gmail.com>
2023-09-18 00:22:35 -0500

    Finished Section 4.42. Converting an Option into a Result

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,6 @@
 use std::str::Utf8Error;
 use std::str;
-use super::method::Method;
+use super::method::{Method,MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
@@ -22,13 +22,23 @@ impl TryFrom<&[u8]> for Request{
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
 
+        let (method,request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path,request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
         unimplemented!()
     }
 }
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
     for (i, c) in request.chars().enumerate() {
-        if c == ' ' {
+        if c == ' ' || c == '\r' {
             return Some((&request[..i], &request[i + 1..]));
         }
     }
@@ -51,6 +61,12 @@ impl ParseError {
             Self::InvalidProtocol=>"Invalid Protocol",
             Self::InvalidMethod=>"Invalid Method",
         }
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 
