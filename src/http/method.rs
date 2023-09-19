ccaffb3d820bcfa978e09e4049b5c5681a307312
Richard Hinojosa <txhc4life@gmail.com>
2023-09-18 22:49:10 -0500

    Finished Section 4.43 Parsing Values from Strings

--- src/http/method.rs
+++ src/http/method.rs
@@ -26,7 +26,7 @@ impl FromStr for Method {
             "OPTIONS" => Ok(Self::OPTIONS),
             "TRACE" => Ok(Self::TRACE),
             "PATCH" => Ok(Self::PATCH),
-            - => Err(MethodError),
+            _ => Err(MethodError),
         }
     }
 }
